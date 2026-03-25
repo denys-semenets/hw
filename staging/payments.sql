222b37ff62f27e869b13fd90cc755d3f22209c76
denys-semenets <dsemenets@kse.org.ua>
2026-03-25 23:21:03 +0200

    Update payments.sql

--- staging/payments.sql
+++ staging/payments.sql
@@ -1,7 +1,8 @@
 select
-    cast(payment_id as int) as payment_id,
-    cast(member_id as int) as member_id,
-    cast(plan_id as int) as plan_id,
+    payment_id as payment_id,
+    member_id as member_id,
+    plan_id as plan_id,
+    amount_cents,
     cast(payment_date as date) as payment_date,
     payment_method
 from {{ ref('raw_payments') }}
